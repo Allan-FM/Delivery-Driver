63e3d8e409356cf3846c4b6a18f5fd1a0b5ecc86
Allan <allanfelipedk@gmail.com>
2022-11-24 11:00:55 -0300

    adding the Transform.Translate method in the player

--- Assets/Runtime/Scripts/PlayerController.cs
+++ Assets/Runtime/Scripts/PlayerController.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speedRotation = 50f;
+    [SerializeField] private float speedRotation = 50f;
     private void Update()
     {
         transform.Rotate(0, 0, speedRotation * Time.deltaTime);
+        transform.Translate(0, 0.01f, 0);
     }
 }
